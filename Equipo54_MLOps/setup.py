962247d8bf88d5ed4575fd8667a5b60050d9e259
dcarloa <dcarloabad@gmail.com>
2025-10-03 17:46:35 -0600

    Template added to Github Solution

---
+++ Equipo54_MLOps/setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Proyecto integrador del equipo 54',
+    author='Equipo 54',
+    license='MIT',
+)
